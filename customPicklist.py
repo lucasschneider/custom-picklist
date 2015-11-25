47608459c0aed66c55d77f9142069686816065d5
Lucas Schneider <lschneider4@wisc.edu>
2015-11-24 20:50:29 -0600

    Added item status and checked out columns

--- customPicklist.py
+++ customPicklist.py
@@ -14,37 +14,17 @@ import sys
 
 ## Class to store picklist items ##
 class Item:
-  def __init__(self, t, c, l, cn, b):
+  def __init__(self, t, c, l, cn, b, s, co):
     self.title = t
     self.collection = c
     self.location = l
     self.callNum = cn
     self.barcode = b
+    self.status = s
+    self.checkedOut = co
 
   def __repr__(self):
     return repr((self.location, self.collection, self.callNum))
-
-  def setTitle(t):
-    self.title = t
-  def setCollection(c):
-    self.collection = c
-  def setLocation(l):
-    self.location = l
-  def setCallNum(cn):
-    self.callNum = cn
-  def setBarcode(b):
-    self.barcode = b
-
-  def getTitle():
-    return str(self.title)
-  def getCollection():
-    return str(self.collection)
-  def getLocation():
-    return str(self.location)
-  def getCallNum():
-    return str(self.callNum)
-  def getBarcode():
-    return str(self.barcode)
 
 ## Function to login into Koha before data extraction ##
 def login():
@@ -134,33 +114,75 @@ collectionIdx = 0
 locationIdx = 0
 callNumIdx = 0
 barcodeIdx = 0
+withdrawnIdx = 0
+damagedIdx = 0
+notForLoanIdx = 0
+checkedOutIdx = 0
 for i in range(len(headers)):
   if headers[i].get_text().strip() == "Title":
     titleIdx = i
-  if headers[i].get_text().strip() == "Collection code":
+  elif headers[i].get_text().strip() == "Collection code":
     collectionIdx = i
-  if headers[i].get_text().strip() == "Shelving location":
+  elif headers[i].get_text().strip() == "Shelving location":
     locationIdx = i
-  if headers[i].get_text().strip() == "Full call number":
+  elif headers[i].get_text().strip() == "Full call number":
     callNumIdx = i
-  if headers[i].get_text().strip() == "Barcode":
+  elif headers[i].get_text().strip() == "Barcode":
     barcodeIdx = i
+  elif headers[i].get_text().strip() == "Withdrawn status":
+    withdrawnIdx = i
+  elif headers[i].get_text().strip() == "Damaged status":
+    damagedIdx = i
+  elif headers[i].get_text().strip() == "Not for loan status":
+    notForLoanIdx = i
+  elif headers[i].get_text().strip() == "Checked out":
+    checkedOutIdx = i
 
 results = soup.find(id="itemst").tbody.find_all('tr')
 pullItems = []
+thisMonth = datetime.date.today().strftime('%m/%Y')
+pullMonth = (datetime.date.today() + relativedelta(months=+6)).strftime('%m/%Y')
+branchCode = str.upper(loginCred[0][:3])
 
 for i in range(len(results)):
   result = results[i].find_all('td')
-  pullItems.append(Item(result[titleIdx].a.get_text(),result[collectionIdx].get_text(),result[locationIdx].get_text(),result[callNumIdx].get_text(),result[barcodeIdx].get_text()))
+  
+  withdrawn = str(result[withdrawnIdx].get_text().strip())
+  damaged = str(result[damagedIdx].get_text().strip())
+  notForLoan = str(result[notForLoanIdx].get_text().strip())
+  itemStatus = "OK"
+  checkedOut = ""
+  
+  if withdrawn and damaged and notForLoan:
+    itemStatus = withdrawn + "<br />" + damaged + "<br />" + notForLoan
+  elif withdrawn and damaged:
+    itemStatus = withdrawn + "<br />" + damaged
+  elif withdrawn and notForLoan:
+    itemStatus = withdrawn + "<br />" + notForLoan
+  elif damaged and notForLoan:
+    itemStatus = damaged + "<br />" + notForLoan
+  elif withdrawn:
+    itemStatus = withdrawn
+  elif damaged:
+    itemStatus = damaged
+  elif notForLoan:
+    itemStatus = notForLoan
+  
+  if checkedOutIdx != 0:
+    checkedOutStr = str(result[checkedOutIdx].get_text().strip())
+    if checkedOutStr:
+      checkedOut = "Yes"
+  pullItems.append(Item(result[titleIdx].a.get_text(),result[collectionIdx].get_text(),result[locationIdx].get_text(),result[callNumIdx].get_text(),result[barcodeIdx].get_text(), itemStatus, checkedOut))
 
 sortedItems = sorted(pullItems, key=attrgetter('location','collection','callNum'))
 
 f = open(str(args.output), 'w')
 f.write("<html>\n<head>\n<style>\nth{font-weight: bold;}td{padding:5px 7px;}</style></head>\n<body>\n")
-f.write("<p>"+datetime.date.today().strftime('This list was generated on:       %x') + '<br />')
-f.write((datetime.date.today() + relativedelta(months=+6)).strftime('These items should be pulled on:  %x') + '</p>')
-f.write("<table>\n<tr>\n<th>Call #</th>\n<th>Title</th>\n<th>Collection</th>\n<th>Location</th>\n<th>Barcode</th>\n</tr>\n")
+f.write("<p>[POOL ITEMS] " + sortedItems[0].collection + "</p>")
+f.write("<p>Arrived at "+ branchCode + " " +  thisMonth + ", to be pulled " + pullMonth + "</p>")
+f.write("<table>\n<tr>\n<th>Call #</th>\n<th>Title</th>\n<th>Barcode</th>\n<th>Item status</th>\n<th>Checked out?</th>\n</tr>\n")
 for item in sortedItems:
-  f.write("<tr>\n<td>"+str(item.callNum)+"</td>\n<td>"+str(item.title)+"</td>\n<td>"+str(item.collection)+"</td>\n<td>"+str(item.location)+"</td>\n<td>"+str(item.barcode)+"</td>\n</tr>\n")
+  
+  f.write("<tr>\n<td>"+str(item.callNum)+"</td>\n<td>"+str(item.title)+"</td>\n<td>"+str(item.barcode)+"</td>\n<td>"+str(item.status)+"</td>\n<td>"+str(item.checkedOut)+"</td>\n</tr>\n")
 f.write("</table>\n</body>\n</html>")
 f.close()
