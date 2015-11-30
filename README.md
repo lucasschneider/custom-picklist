dff6bcbef63c71bcbd8f96c6bdfe4c0ce203d7c9
Lucas Schneider <lrschneider94@gmail.com>
2015-11-29 20:34:16 -0600

    Added GUI input file select, automatically open web browser with output

--- README.md
+++ README.md
@@ -9,14 +9,16 @@ This script generates a pull list HTML file from a plain text file of barcode nu
 ```
 poolPL.py [-h] [-b BRANCHCODE] -i INPUT [-o OUTPUT]
 
-Arguments:
+Optional rguments:
 -h, --help        Show this help message and exit
 -b BRANCHCODE, --branchcode BRANCHCODE
-                  Override the branch code for the library at which the picklist was generated
+                  Override the branch code for the library at which the picklist was generated;
+                  default based on the Koha login username
 -i INPUT, --input INPUT
                   Input text filename; This file's extension must end in .txt and the provided filename
-                  will be relative to the present working directory
+                  will be relative to the present working directory; default based on on GUI input
 -o OUTPUT, --output OUTPUT
                   Output web filename; This file's extension will end in .html whether or not it is
-                  specified and will be created relative to the location of the input file
+                  specified and will be created relative to the location of the input file; default
+                  saved to same directory as filepath
 ```
--- poolPL.py
+++ poolPL.py
@@ -10,6 +10,15 @@ from operator import itemgetter, attrgetter, methodcaller
 from bs4 import BeautifulSoup
 import sys
 import os
+import re
+import webbrowser
+new = 2 # open in a new tab, if possible
+
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
 
 directorySlash = '\\' if os.name == 'nt' else '/'
 
@@ -40,20 +49,26 @@ def login():
 ## Parse for proper argumets ##
 parser = argparse.ArgumentParser(description='This script generates a pull list HTML file from a plain text file of barcode numbers. Each library barcode number should be given its own line without punctuation. This is intended for use by LibLime Koka users within the South Central Library System in Wisconsin.')
 parser.add_argument('-b','--branchcode',help='Override the library branch code', nargs='?')
-parser.add_argument('-i','--input', help='Input text filename (.txt); Relative to the present working directory',required=True)
+parser.add_argument('-i','--input', help='Input text filename (.txt); Relative to the present working directory')
 parser.add_argument('-o','--output',help='Output web filename (.html); Relative to the input file')
 args = parser.parse_args()
 
-inFile = str(args.input)
+inFile = args.input
+
+while inFile is None:
+  inFile = filedialog.askopenfilename()
+  if not inFile:
+    sys.exit(0)
+print(inFile)
 
 if inFile[-4:] != '.txt':
-  print("The file \"" + os.getcwd() + directorySlash + inFile + "\" does not appear to be a text file.\nPlease check the file type (.txt) and try again.")
-  sys.exit(0)
+    print("The file \"" + os.path.realpath(inFile) + "\" does not appear to be a text file.\nPlease check the file type (.txt) and try again.")
+    sys.exit(0)
   
 try:
   barcodes = [x.strip('\n') for x in open(inFile).readlines()]
-except IOError:
-  print("The text file \"" + os.getcwd() + directorySlash + inFile + "\" could not be found.\nPlease try again with a valid filepath.") 
+except IOError:  
+  print("The text file \"" + os.path.realpath(inFile) + "\" could not be found.\nPlease try again with a valid filepath.") 
   sys.exit(0)
 
 while True:
@@ -100,7 +115,8 @@ while True:
 barcodes = list(filter(None, barcodes))
 barcodeList = ""
 for barcode in barcodes:
-  barcodeList += barcode + '\r\n'
+  if re.match("\d{14}",barcode):
+    barcodeList += barcode + '\r\n'
 
 # Input parameters we are going to send
 payload = {
@@ -165,14 +181,14 @@ branchCode = str.upper(loginCred[0][:3]) if not args.branchcode else args.branch
 
 for i in range(len(results)):
   result = results[i].find_all('td')
-  
+
   withdrawn = str(result[withdrawnIdx].get_text().strip()) if withdrawnIdx != 0 else ""
   lost = str(result[lostIdx].get_text().strip()) if lostIdx != 0 else ""
   damaged = str(result[damagedIdx].get_text().strip()) if damagedIdx != 0 else ""
   otherStatus = str(result[otherStatusIdx].get_text().strip()) if otherStatusIdx != 0 else ""
   notForLoan = str(result[notForLoanIdx].get_text().strip())  if notForLoanIdx != 0 else ""
   checkedOut = ""
-  
+
   itemStatus = withdrawn;
   itemStatus += "<br />" + lost if itemStatus else lost
   itemStatus += "<br />" + damaged if itemStatus else damaged
@@ -206,5 +222,7 @@ for item in sortedItems:
   f.write("<tr>\n<td>"+str(item.callNum)+"</td>\n<td>"+str(item.title)+"</td>\n<td>"+str(item.barcode)+"</td>\n<td>"+str(item.status)+"</td>\n<td>"+str(item.checkedOut)+"</td>\n</tr>\n")
 f.write("</table>\n</body>\n</html>")
 f.close()
+
+webbrowser.open(outFile,new=new)
 
 print ('Output saved to:\n' + outFile)
